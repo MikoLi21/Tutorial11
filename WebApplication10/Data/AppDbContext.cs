1a16f009e4a1d72b9ec44ad4642b27a1b151c837
s30919 <s30919@pjwstk.edu.pl>
2025-05-25 21:07:28 +0300

    EF Core migration

--- WebApplication10/Data/AppDbContext.cs
+++ WebApplication10/Data/AppDbContext.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApplication10.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication10.Models;
 
-public class AppDbContext
+namespace WebApplication10.Data
 {
-    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
+
+            
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasOne(pm => pm.Prescription)
+                .WithMany(p => p.PrescriptionMedicaments)
+                .HasForeignKey(pm => pm.IdPrescription);
+
+            
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasOne(pm => pm.Medicament)
+                .WithMany(m => m.PrescriptionMedicaments)
+                .HasForeignKey(pm => pm.IdMedicament);
+        }
+    }
 }
