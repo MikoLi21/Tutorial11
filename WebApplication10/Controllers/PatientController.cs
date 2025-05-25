8730f9405b06dd761dff96f86648858388736feb
s30919 <s30919@pjwstk.edu.pl>
2025-05-25 21:34:58 +0300

    Get

--- WebApplication10/Controllers/PatientController.cs
+++ WebApplication10/Controllers/PatientController.cs
@@ -1,6 +1,28 @@
-﻿namespace WebApplication10.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Services;
 
-public class PatientController
+namespace WebApplication10.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientController : ControllerBase
+    {
+        private readonly IPatientService _service;
+
+        public PatientController(IPatientService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPatient(int id)
+        {
+            var result = await _service.GetPatientAsync(id);
+
+            if (result == null)
+                return NotFound($"Patient with ID {id} not found.");
+
+            return Ok(result);
+        }
+    }
 }
--- WebApplication10/Program.cs
+++ WebApplication10/Program.cs
@@ -5,7 +5,7 @@ using WebApplication10.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
-
+builder.Services.AddScoped<IPatientService, PatientService>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
